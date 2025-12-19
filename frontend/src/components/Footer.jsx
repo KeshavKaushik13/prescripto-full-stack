3882240aa89029c5b9a30b2b202891395d8a08cc
KeshavKaushik13 <keshavkaushik13112004@gmail.com>
2025-12-19 11:16:58 +0530

    Update footer content

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,8 @@ const Footer = () => {
 
         <div>
           <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>Prescripto is a modern healthcare platform designed to simplify online doctor appointment booking. Prescripto helps patients find trusted doctors and manage appointments efficiently in one place. The platform provides an intuitive interface for users and administrators, enabling smooth scheduling, secure access, and practical management of healthcare services.
+</p>
         </div>
 
         <div>
@@ -24,8 +25,8 @@ const Footer = () => {
         <div>
           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
           <ul className='flex flex-col gap-2 text-gray-600'>
-            <li>+1-212-456-7890</li>
-            <li>greatstackdev@gmail.com</li>
+            <li>+91-212-456-7890</li>
+            <li>prescriptohelp@gmail.com</li>
           </ul>
         </div>
 
